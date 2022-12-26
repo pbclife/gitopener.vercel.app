f5a252bf4c562a3e701fdb8068769d95cba4cf1f
Sagar Bera <contact.sagarbera@gmail.com>
2022-12-27 00:32:06 +0530

    build: configured vscode settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
+import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
