6d86bd2b2ac1e4e7555cd35e22668a2492dcbb96
Sagar Bera <contact.sagarbera@gmail.com>
2022-12-27 01:50:50 +0530

    style: implemented tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css';
+import { Fira_Code, Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+const inter = Inter({
+  display: 'block',
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+const firaCode = Fira_Code({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-fira-code',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <main className={`${inter.variable} ${firaCode.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '375px',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-fira-code)', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
