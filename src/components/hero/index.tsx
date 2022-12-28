c60099a804bbfe2c17a6e5d38ad97ccc2dbb15cb
Sagar Bera <contact.sagarbera@gmail.com>
2022-12-28 22:32:32 +0530

    style: header designed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import TextSection from './text-section';
 
 export default function Hero() {
   return (
-    <div className="flex w-full items-center gap-4 pb-6">
+    <div className="flex w-full items-center justify-between pb-6">
       <TextSection />
       <LogoSection />
     </div>
