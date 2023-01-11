725d86e9835733a21c128d92eec405e690984463
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-12 01:26:00 +0530

    build: updated configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const mdx = require('@next/mdx');
+const withMdx = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withMdx({
   reactStrictMode: true,
-};
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+});
 
 module.exports = nextConfig;
