2bc3518eac361c814faf68c16df0ef87227be26a
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-12 23:23:25 +0530

    feat: added poping component utiity

---
+++ src/components/utilities/pop-button.tsx
@@ -0,0 +1,24 @@
+import { Menu } from '@headlessui/react';
+import { FC, ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  className: string;
+  onClick?: () => void;
+};
+
+const PopButton: FC<Props> = ({ children, onClick, className }) => {
+  return (
+    <Menu.Item
+      as="button"
+      onClick={onClick}
+      className={`font-lexend rounded-full px-6 py-2 font-semibold capitalize outline-none ${
+        className || ``
+      }`}
+    >
+      {children}
+    </Menu.Item>
+  );
+};
+
+export default PopButton;
