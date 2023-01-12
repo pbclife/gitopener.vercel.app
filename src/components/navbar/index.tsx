3733ef2704924ec3f2c35a68985a43f013905ee4
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-12 23:32:03 +0530

    feat: implemented theme switcher

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,24 +1,25 @@
 import GitOpenerIcon from '@/components/icons/git-opener';
+import { useThemeContext } from '@/context/theme-context';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
 import Container from '@layouts/container';
+import Pop from '@utilities/pop';
+import Themes from './theme';
 
 export default function Navbar() {
+  const { isDark } = useThemeContext();
   return (
-    <Container>
+    <Container className="z-10 flex items-center justify-between">
       {/* logo */}
-      <div className="flex items-center gap-x-2 py-4">
-        {/* <Image
-          src={PBCLifeLogo}
-          alt="logo"
-          placeholder="blur"
-          width={48}
-          height={48}
-          className="h-10 w-10 rounded-full object-cover object-center"
-        /> */}
+      <div className="flex items-center gap-x-2 py-4 text-skin-base">
         <GitOpenerIcon className="h-10 w-10 rotate-90 " />
         <span className="text-2xl font-bold tracking-tight">Git Opener</span>
       </div>
       {/* social icons */}
-      <div className=""></div>
+      <div className="">
+        <Pop Icon={isDark ? MoonIcon : SunIcon} className="!z-50">
+          <Themes />
+        </Pop>
+      </div>
     </Container>
   );
 }
