e69f8f3fda0a5844534edbe6481d1bc86a84d8c1
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-14 01:43:46 +0530

    refactor: separated theme-button from navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,22 +1,14 @@
-import { useThemeContext } from '@/context/theme-context';
-import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
 import Container from '@layouts/container';
 import Logo from '@utilities/logo';
-import Pop from '@utilities/pop';
-import Themes from './theme';
+import ThemeButton from './theme-button';
 
 export default function Navbar() {
-  const { isDark } = useThemeContext();
   return (
     <Container className="z-10 flex items-center justify-between">
       {/* logo */}
       <Logo />
       {/* social icons */}
-      <div className="">
-        <Pop Icon={isDark ? MoonIcon : SunIcon} className="!z-50">
-          <Themes />
-        </Pop>
-      </div>
+      <ThemeButton />
     </Container>
   );
 }
---
+++ src/components/navbar/theme-button.tsx
@@ -0,0 +1,15 @@
+import { useThemeContext } from '@/context/theme-context';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+import Pop from '@utilities/pop';
+import Themes from './theme';
+
+export default function ThemeButton() {
+  const { isDark } = useThemeContext();
+  return (
+    <div className="">
+      <Pop Icon={isDark ? MoonIcon : SunIcon} className="!z-50">
+        <Themes />
+      </Pop>
+    </div>
+  );
+}
