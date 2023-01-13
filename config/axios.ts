993fa70c50c0e307adfc4e7c928e060c72da588f
Sagar Bera <86586277+sboy99@users.noreply.github.com>
2023-01-13 15:34:14 +0530

    Bug-Fix: Fixed sync with system on theme change (#2)
    * fix: fixed sync with system issue
    
    * fix: fixed axios data fetching issue in production
    
    * style: made theme button square rounded
    
    * fix: redefined query string

--- config/axios.ts
+++ config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === `production`
-    ? `https://pbclife.vercel.app/api/v1`
+    ? `https://gitopener.vercel.app/api/v1`
     : `http://localhost:3000/api/v1`;
 
 export default axios.create({
