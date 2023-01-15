90457d398d0540fdbfbf23812c07a8b2d7f522b1
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-15 18:24:29 +0530

    style: restructed and designed contributor page

--- src/components/contributorpage/ContributorComp.tsx
+++ src/components/contributorpage/ContributorComp.tsx
@@ -2,8 +2,14 @@ import type { TCont } from '&validation/contributor.validation';
 import { getFormattedDate } from 'lib/date';
 import Image from 'next/image';
 import { ComponentProps, FC } from 'react';
+import GithubIcon from '../icons/github';
+import Glow from '../utilities/glow';
 
-type ContributorProps = TCont & ComponentProps<'div'>;
+type ContributorProps = TCont &
+  ComponentProps<'div'> & {
+    glow?: boolean;
+    displayFollowers?: boolean;
+  };
 
 const ContributorComp: FC<ContributorProps> = ({
   avatar_url,
@@ -13,17 +19,22 @@ const ContributorComp: FC<ContributorProps> = ({
   occupation,
   createdAt,
   className,
+  glow = false,
+  followers,
   ...props
 }) => {
   const contributedIn = getFormattedDate(createdAt);
 
   return (
     <div
-      className={`group relative mx-auto h-fit w-full divide-y divide-skin-base rounded-md border border-skin-base bg-skin-base p-4 text-skin-base transition-colors duration-200 ease-in-out hover:bg-skin-shine sm:max-w-md sm:p-6 ${
+      className={`group relative mx-auto h-fit w-full  rounded-md border ${
+        glow ? ` border-accent ` : `border-skin-base `
+      }  bg-skin-base p-4 text-skin-base transition-colors duration-200 ease-in-out hover:border-accent hover:bg-skin-shine sm:max-w-md sm:p-6 ${
         className || ``
       }`}
       {...props}
     >
+      {glow && <Glow className="w-full" />}
       {/* head */}
       <div className={`flex items-center gap-x-4 ${bio && `pb-4`}`}>
         {/* profile picture */}
@@ -42,14 +53,33 @@ const ContributorComp: FC<ContributorProps> = ({
           </strong>
           {/* occupation */}
           <p className="text-sm font-medium text-skin-muted transition-colors duration-200 ease-in-out group-hover:text-skin-base ">
-            {occupation}
+            {followers && followers > 0 ? (
+              <span className={`flex items-center gap-x-2 `}>
+                <GithubIcon className="h-5 w-5 text-skin-base" />
+                {followers} followers
+              </span>
+            ) : (
+              occupation
+            )}
           </p>
         </div>
       </div>
       {/* bio */}
-      {bio && <p className="py-2 font-medium ">{bio}</p>}
+      {bio && (
+        <p
+          className={`border-t group-hover:border-accent ${
+            glow ? `border-accent` : `border-skin-base `
+          } py-2 font-medium`}
+        >
+          {bio}
+        </p>
+      )}
       {/* contribution date */}
-      <div className="absolute right-2 top-1 border-none text-xs capitalize text-skin-base/30">
+      <div
+        className={`absolute right-2 top-1 border-transparent text-xs capitalize ${
+          glow ? `text-accent/70` : `text-skin-base/30`
+        }`}
+      >
         {contributedIn}
       </div>
     </div>
--- src/components/layouts/represent/index.tsx
+++ src/components/layouts/represent/index.tsx
@@ -16,16 +16,22 @@ const Represent: FC<RepresentProps> = ({
   ...props
 }) => {
   return (
-    <section className="relative space-y-4 py-2">
+    <section className="relative z-0 space-y-4 py-2">
+      <div className="pointer-events-none absolute inset-y-0 left-4 -z-10 hidden translate-y-10 border-l border-accent xs:block" />
+
       {/* typo */}
       <TypoComp className=" prose-base prose-h1:my-0 prose-h4:text-accent prose-p:leading-6 sm:prose-lg sm:prose-p:leading-7">
-        <h4 className={`flex items-end gap-2`}>
-          {TopIcon && <TopIcon className="h-8 w-8" />}
+        <h4 className={`flex items-center gap-2`}>
+          {TopIcon && (
+            <TopIcon className="h-8 w-8 rounded-full border border-accent bg-accent p-1 !text-white" />
+          )}
           <span>{topTitle}</span>
         </h4>
-        <h1>{mainTitle}</h1>
+        <div className="pl-0 xs:pl-9 sm:pl-10">
+          <h1>{mainTitle}</h1>
 
-        <p>{about}</p>
+          <p>{about}</p>
+        </div>
       </TypoComp>
       {/* body */}
       <div {...props} />
