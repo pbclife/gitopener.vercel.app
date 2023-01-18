60179a1fe1512a481c49ba37625bfa713886a337
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-18 18:01:02 +0530

    feat: added external links

---
+++ src/data/HeroLinks.ts
@@ -0,0 +1,19 @@
+import ForkIcon from '@/components/icons/fork';
+import GithubIcon from '@/components/icons/github';
+
+const HeroLinks = [
+  {
+    id: 1,
+    Icon: ForkIcon,
+    title: `Fork`,
+    href: `https://github.com/pbclife/gitopener.vercel.app/fork`,
+  },
+  {
+    id: 2,
+    Icon: GithubIcon,
+    title: `Repository`,
+    href: `https://github.com/pbclife/gitopener.vercel.app`,
+  },
+];
+
+export default HeroLinks;
