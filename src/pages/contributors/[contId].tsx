ac9bcf436c81dccac3d3b379575d40e8c133e93e
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-06 15:23:06 +0530

    style: started designig contributor profile page

--- src/pages/contributors/[contId].tsx
+++ src/pages/contributors/[contId].tsx
@@ -1,20 +1,40 @@
+import type { TContributor } from '@/types/server/Contributors';
+import type { GetStaticPaths, GetStaticProps } from 'next';
+import type { FC } from 'react';
+
+import ContributorProfilePageLayout from '@/layouts/ContributorProfilePageLayout';
 import {
   fetchSingleContributor,
   getContribution,
   getDynamicPaths,
 } from '@/lib/contributors';
-import type { TContributor } from '@/types/server/Contributors';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import type { FC } from 'react';
+
+import mdxComponents from '@/components/mdxcomponents';
+import TypoComp from '@/components/utilities/TypoComponent';
+import { Contribution } from '@/types/client/Contributors';
+import { MDXRemote } from 'next-mdx-remote';
 
 type SingleContributorProps = {
   contributor: Omit<TContributor, 'isDeleted'> & {
     createdAt?: Date;
   };
+  contribution: Contribution;
 };
 
-const SingleContributor: FC<SingleContributorProps> = ({ contributor }) => {
-  return <>single contributor {contributor.name} </>;
+const SingleContributor: FC<SingleContributorProps> = ({
+  contributor,
+  contribution,
+}) => {
+  return (
+    <ContributorProfilePageLayout
+      meta={contribution.meta}
+      contributor={contributor}
+    >
+      <TypoComp className="max-w-full">
+        <MDXRemote {...contribution.source} components={mdxComponents} />
+      </TypoComp>
+    </ContributorProfilePageLayout>
+  );
 };
 
 export default SingleContributor;
@@ -35,18 +55,18 @@ export const getStaticProps: GetStaticProps<SingleContributorProps> = async ({
     if (!params?.contId || Array.isArray(params.contId))
       throw new Error(`Please specify a single contributor id`);
     const contribution = await getContribution(params.contId); // autometically throws error if file not found
-    const contributor = await fetchSingleContributor(
-      contribution.meta.github_username as string,
-      contribution
-    );
+    const contributor = await fetchSingleContributor(contribution.meta);
 
     return {
       props: {
         contributor,
+        contribution,
       },
       revalidate: 15,
     };
   } catch (error) {
+    console.log(error);
+
     return {
       notFound: true,
     };
