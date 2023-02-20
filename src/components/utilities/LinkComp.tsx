8b429cb66869cab85c5f3e0a4bfaa8c3be403872
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-21 01:37:23 +0530

    chore: updated utility components

--- src/components/utilities/LinkComp.tsx
+++ src/components/utilities/LinkComp.tsx
@@ -4,15 +4,28 @@ import type { FC, ReactNode } from 'react';
 type LinkCompProps = {
   href?: string;
   children: ReactNode;
+  isCardComp?: boolean;
 };
 
-const LinkComp: FC<LinkCompProps> = ({ href, children }) => {
+const LinkComp: FC<LinkCompProps> = ({
+  href,
+  children,
+  isCardComp = false,
+}) => {
   const isInternal = href?.startsWith('/') ?? false;
   return href ? (
     isInternal ? (
       <Link href={href}>{children}</Link>
     ) : (
-      <a href={href} target="_blank" rel="noreferrer">
+      <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className={`${
+          isCardComp &&
+          `rounded-lg border border-transparent p-0 hover:border-accent prose-p:font-normal`
+        }`}
+      >
         {children}
       </a>
     )
