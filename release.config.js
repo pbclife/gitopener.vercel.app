ef3446f999c4d0d869e2754bf5b702d4a4693161
Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>
2023-03-18 19:55:35 +0530

    ci: added github action intigations (#88)
    * fix: upgraded to the latest version🌈
    
    * added my profile ð
    
    * added greetings action
    
    * feat: added semantic release
    
    * added sem release action
    
    * added dependabot
    
    * Update release.yml
    
    * Delete package-lock.json
    
    ---------
    
    Co-authored-by: Sagar Bera <86586277+sboy99@users.noreply.github.com>

---
+++ release.config.js
@@ -0,0 +1,19 @@
+const config = {
+  branches: ['main'],
+  Plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
+
+module.exports = config;
